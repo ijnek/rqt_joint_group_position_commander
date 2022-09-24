e30b93d4cf61335e9b25962cf4736d66e016c59f
Kenji Brameld <kenjibrameld@gmail.com>
2022-09-24 06:14:13 +0000

    implement joint detection, and slider creation
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- rqt_joint_group_position_commander/rqt_joint_group_position_commander.py
+++ rqt_joint_group_position_commander/rqt_joint_group_position_commander.py
@@ -2,14 +2,25 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QSlider, QWidget
-from std_msgs.msg import Float64MultiArray
+from python_qt_binding.QtCore import pyqtSignal, pyqtSlot, Qt
+from python_qt_binding.QtWidgets import QLabel, QSlider, QWidget
+from qt_gui.plugin import Plugin
+from rclpy.qos import DurabilityPolicy, QoSProfile, ReliabilityPolicy
+from std_msgs.msg import Float64MultiArray, String
+from typing import List, Tuple
+from urdf_parser_py.urdf import URDF
 
 class RqtJointGroupPositionCommander(Plugin):
 
-    _joint_states = [0.0, 0.0, 0.0]
+    # A value to divide the slider value by to get the joint position.
+    # This is necessary because slider values only allow integers and not floats.
+    SLIDER_MULTIPLIER = 1000
+
+    _joints: URDF = None
+    _sliders: List[Tuple[QLabel, QSlider]] = []
+
+    update_joints = pyqtSignal()
 
     def __init__(self, context):
         super().__init__(context)
@@ -37,14 +48,19 @@ class RqtJointGroupPositionCommander(Plugin):
         # Add widget to the user interface
         context.add_widget(self._widget)
 
-        # Connect callbacks
-        self._widget.slider_1.valueChanged.connect(self.callback_joint_1)
-        self._widget.slider_2.valueChanged.connect(self.callback_joint_2)
-        self._widget.slider_3.valueChanged.connect(self.callback_joint_3)
+        # Connect signal
+        self.update_joints.connect(self._update_sliders)
 
         # Create publisher
         self._cmd_pub = context.node.create_publisher(
             Float64MultiArray, '/joint_group_position_controller/commands', 1)
+
+        # Create subscription
+        qos_profile = QoSProfile(depth=1)
+        qos_profile.durability = DurabilityPolicy.TRANSIENT_LOCAL
+        qos_profile.reliability = ReliabilityPolicy.RELIABLE
+        self._robot_description_sub = context.node.create_subscription(
+            String, '/robot_description', self._callback_robot_description, qos_profile)
 
     def shutdown_plugin(self):
         # TODO unregister all publishers here
@@ -56,19 +72,31 @@ class RqtJointGroupPositionCommander(Plugin):
     def restore_settings(self, plugin_settings, instance_settings):
         pass
 
-    def callback_joint_1(self, val):
-        self._joint_states[0] = val / 1000.0
-        self.publish()
-
-    def callback_joint_2(self, val):
-        self._joint_states[1] = val / 1000.0
-        self.publish()
-
-    def callback_joint_3(self, val):
-        self._joint_states[2] = val / 1000.0
-        self.publish()
-
     def publish(self):
         msg = Float64MultiArray()
-        msg.data = self._joint_states
+        for slider in self._sliders:
+            msg.data.append(float(slider.value()) / self.SLIDER_MULTIPLIER)
         self._cmd_pub.publish(msg)
+
+    def _callback_robot_description(self, msg: String):
+        robot = URDF.from_xml_string(msg.data)
+        self._joints = robot.joints
+        self.update_joints.emit()
+
+    @pyqtSlot()
+    def _update_sliders(self):
+        self._sliders.clear()
+        for joint in self._joints:
+            if joint.type == 'revolute':
+                self._add_slider(joint.name, joint.limit.lower, joint.limit.upper)
+
+    def _add_slider(self, name: str, minimum: float, maximum: float):
+        label = QLabel(name)
+
+        slider = QSlider(Qt.Orientation.Horizontal)
+        slider.setMinimum(minimum * self.SLIDER_MULTIPLIER)
+        slider.setMaximum(maximum * self.SLIDER_MULTIPLIER)
+        slider.valueChanged.connect(self.publish)
+
+        self._widget.layout().addRow(label, slider)
+        self._sliders.append(slider)
